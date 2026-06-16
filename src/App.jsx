49ed496c69907bccb130e1aa62fe86c3767db295
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-16 10:55:16 +0700

    feat: started react template

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-slate-900 text-white">
+        <div className="text-center space-y-4">
+          <h1 className="text-5xl font-extrabold tracking-tight bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
+            React + Tailwind CSS v4
+          </h1>
+          <p className="text-slate-400 text-lg">
+            Setup terbaru, super cepat, tanpa file konfigurasi ribet!
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
